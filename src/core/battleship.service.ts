f6f974d4b4d63f39df3fe701849c28ff8a374f18
Loïc Le Goff <loic.legoff@healthhero.com>
2023-04-26 17:20:43 +0200

    add type and ShipViewer

--- src/core/battleship.service.ts
+++ src/core/battleship.service.ts
@@ -1,9 +1,14 @@
+export interface Ship {
+  key: string;
+  length: number;
+  name: string;
+}
 export class BattleshipService {
   static async getShips() {
     // https://monurl/api/reference/ships
 
     const resp = await fetch('/api/reference/ships');
     const data = await resp.json();
-    return data;
+    return data as Ship[];
   }
 }
--- src/pages/preparation.tsx
+++ src/pages/preparation.tsx
@@ -1,8 +1,17 @@
-import { BattleshipService } from '@/core/battleship.service';
+import { BattleshipService, Ship } from '@/core/battleship.service';
 import { useEffect, useState } from 'react';
 
+interface ShipViewerProps {
+  length: number;
+  name: string;
+}
+
+function ShipViewer(props: ShipViewerProps) {
+  return <div>{props.name}</div>;
+}
+
 export default function Preparation() {
-  const [ships, setShips] = useState([]);
+  const [ships, setShips] = useState<Ship[]>([]);
 
   useEffect(() => {
     async function getShips() {
@@ -13,5 +22,11 @@ export default function Preparation() {
     getShips();
   }, []);
 
-  return <div>{JSON.stringify(ships)}</div>;
+  return (
+    <div>
+      {ships.map((ship) => (
+        <ShipViewer name={ship.name} length={ship.length} />
+      ))}
+    </div>
+  );
 }
